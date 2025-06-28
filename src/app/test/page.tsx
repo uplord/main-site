ddbe067d71f46da24117213edeae7988b5c85eee
Michael <michael@uplord.co.uk>
2025-06-28 01:13:07 +0100

    feat: add images

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,6 +2,7 @@
 
 import NiceModal from '@ebay/nice-modal-react'
 import { clsx } from 'clsx'
+import { useTheme } from 'next-themes'
 import dynamic from 'next/dynamic'
 import React, { FC } from 'react'
 import { Header, Example, Button } from 'uplord-ui'
@@ -13,10 +14,19 @@ const ContactFormModal = dynamic(() => import('@/components/modals/ContactForm')
 
 export default function TestPage() {
   const mounted = useMounted()
+  const { resolvedTheme, setTheme } = useTheme()
+
+  const handleToggleTheme = (theme: 'dark' | 'light') => {
+    setTheme(theme)
+  }
 
   return (
     <div className={styles.page}>
-      <Header />
+      <Header
+        id="header"
+        theme={resolvedTheme}
+        onToggleTheme={handleToggleTheme}
+      />
       <main className={styles.main}>
         <div className={styles.section}>
           <div className={styles.container}>
