c1c7a0ead4d0bd3f65bcb7b834f4199ff197b9fb
Michael <michael@uplord.co.uk>
2025-08-02 20:21:57 +0100

    feat: viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,6 @@ export const metadata = {
     icon: '/favicon.svg',
     apple: '/apple.png',
   },
-  viewport: 'width=device-width, initial-scale=1, viewport-fit=cover',
 }
 
 export default function RootLayout({
@@ -37,6 +36,12 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning>
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+      </head>
       <body
         className={`${inter.className} ${nunito.className}`}
         suppressHydrationWarning>
