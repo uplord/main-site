37b16ec9e5dc7d58b045327daf986140b89b4c4a
Michael <michael@uplord.co.uk>
2025-08-02 20:09:33 +0100

    feat: viewport update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export const metadata = {
     icon: '/favicon.svg',
     apple: '/apple.png',
   },
+  viewport: 'width=device-width, initial-scale=1, viewport-fit=cover',
 }
 
 export default function RootLayout({
